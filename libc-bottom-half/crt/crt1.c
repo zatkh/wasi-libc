71d07cbbf0bcc354157126d8de2275bc569e31cc
Dan Gohman <sunfish@mozilla.com>
2020-06-11 13:50:30 -0700

    New-style command support.
    This enables [new-style command support]. Instead of calling
    `__wasm_call_ctors` and `__wasm_call_dtors` directly, this lets
    wasm-ld automatically call them.
    
    [new-style command support]: https://reviews.llvm.org/D81689

--- libc-bottom-half/crt/crt1.c
+++ libc-bottom-half/crt/crt1.c
@@ -2,23 +2,17 @@
 extern void __wasm_call_ctors(void);
 extern int __original_main(void);
 extern void __wasm_call_dtors(void);
+_Noreturn void exit(int);
 
 __attribute__((export_name("_start")))
 void _start(void) {
-    // The linker synthesizes this to call constructors.
-    __wasm_call_ctors();
-
     // Call `__original_main` which will either be the application's zero-argument
     // `__original_main` function or a libc routine which calls `__main_void`.
     // TODO: Call `main` directly once we no longer have to support old compilers.
     int r = __original_main();
 
-    // Call atexit functions, destructors, stdio cleanup, etc.
-    __wasm_call_dtors();
-
-    // If main exited successfully, just return, otherwise call
-    // `__wasi_proc_exit`.
+    // If main exited successfully, just return, otherwise call `exit`.
     if (r != 0) {
-        __wasi_proc_exit(r);
+        exit(r);
     }
 }
